dba6b19cbaedac9e4e09903988946374b54d971d
Adamimoka <adamimoka@gmail.com>
2024-12-11 21:06:14 -0500

    main part of game working

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
-let wordList = ['cat', 'dog', 'bird', 'fish', 'elephant', 'giraffe', 'zebra', 'lion', 'tiger', 'bear']; // testing for now, will open a massive file later
-let commonWordList = ['this file should contain a smaller number of common words, maybe the top 1000 most common english words'];
+const wordList = new Set(['cat', 'dog', 'bird', 'fish', 'elephant', 'giraffe', 'zebra', 'lion', 'tiger', 'bear']); // testing for now, will open a massive file later
+const commonWordList = new Set(['this file should contain a smaller number of common words, maybe the top 1000 most common english words']);
+const usedWords = new Set([]);
 
-let usedWords = [];
 let PromptList = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 let wordPrompt = '';
 
+let problemMessage = '';
+let problemOpacity = 0;
+let problemColor = 'red';
+
 let score = 0;
-let timeToGuess = 10;
-let wordsLeftWithThisTimeAmount = 5; // Counts down the number of words left until the time amount is reduced
 let timer = 10;
 let playing = false;
 
@@ -22,6 +24,10 @@ setInterval(function() {
         if (timer <= 0) {
             gameOver();
         }
+        problemOpacity = Math.max(problemOpacity - 0.02, 0);
+        document.getElementById('problem').innerText = problemMessage;
+        document.getElementById('problem').style.opacity = problemOpacity;
+        document.getElementById('problem').style.color = problemColor;
     }
 }, 20);
 
@@ -34,73 +40,80 @@ function checkWord() {
     typedWord = typedWord.toLowerCase();
     typedWord = typedWord.replace(/[^a-z]/g, ''); // Remove all non-alphabet characters
 
-    if (wordList.includes(typedWord) && !usedWords.includes(typedWord)) { // Available typedWord
-        usedWords.push(typedWord);
-        score++;
-        document.getElementById('score').innerText = score;
-        updateTime();
-        promptIndex = Math.floor(Math.random() * PromptList.length);
-        wordPrompt = PromptList[promptIndex];
-        
+    if (!wordList.has(typedWord)) { // Not a real word
+        problemMessage = `'${typedWord}' is not a word!`;
+        problemOpacity = 1;
+        problemColor = 'red';
+        return;
     }
-    else if (usedWords.includes(typedWord)) { // Already used typedWord
-        alert('You already used that word!');
+
+    if (!typedWord.includes(wordPrompt)) { // Incorrect prompt
+        problemMessage = `'${typedWord}' does not have the prompt '${wordPrompt}'!`;
+        problemOpacity = 1;
+        problemColor = 'red';
+        return;
     }
-    else { // Incorrect typedWord
-        alert('That is not a word!');
+
+    if (usedWords.has(typedWord)) { // Word already used
+        problemMessage = `'${typedWord}' has already been used!`;
+        problemOpacity = 1;
+        problemColor = 'red';
+        return;
     }
+
+    scorePoint();
+}
+
+function scorePoint() {
+    score++;
+    updateTime();
+    usedWords.add(typedWord);
+
+    document.getElementById('score').innerText = score;
+    document.getElementById('timer').innerText = timer.toFixed(2);
+    document.getElementById('wordInput').value = '';
+
+    let promptIndex = Math.floor(Math.random() * PromptList.length);
+    wordPrompt = PromptList[promptIndex];
+    document.getElementById('prompt').innerText = wordPrompt;
 }
 
 function updateTime() {
-    if (wordsLeftWithThisTimeAmount > 0) {
-        wordsLeftWithThisTimeAmount--;
-    }
-    else {
-        if (timeToGuess > 4) {
-            timeToGuess--;
-        }
-        else if (timeToGuess > 2) {
-            timeToGuess -= 0.5;
-        }
-        else if (timeToGuess > 1) {
-            timeToGuess -= 0.2;
-        }
-        else if (timeToGuess > 0.5) {
-            timeToGuess -= 0.1;
-        }
-        // else {
-        //     timeToGuess = 0.5;
-        // }
-        wordsLeftWithThisTimeAmount = 5;
-    }
-    timer = timeToGuess;
+    timer = Math.max(10.5 * 0.94 ** score, 0.5);
 }
 
 function startGame() {
-    usedWords = [];
+    usedWords.clear();
+
     wordPrompt = 'e';
+
     score = 0;
-    timeToGuess = 10;
-    wordsLeftWithThisTimeAmount = 5;
     timer = 10;
+    playing = true;
 
     document.getElementById('score').innerText = score;
-    document.getElementById('timer').innerText = timer;
+    document.getElementById('timer').innerText = timer.toFixed(2);
+    document.getElementById('prompt').innerText = wordPrompt;
+    document.getElementById('wordInput').value = '';
     
     playing = true;
 }
 
 function gameOver() {
     playing = false;
-    console.log('Game Over');
-    let potentialWords = wordList.filter(word => !usedWords.includes(word)); // <-- verify this works
+    const potentialWords = (wordList.difference(commonWordList)).difference(usedWords);
+    const potentialCommonWords = commonWordList.difference(usedWords);
     
     function getRandomElements(arr, numElements) {
         const shuffled = [...arr].sort(() => 0.5 - Math.random());
         return shuffled.slice(0, numElements);
       }
     
-    let randomPotentialWords = getRandomElements(potentialWords, 3);
+    let randomPotentialCommonWords = getRandomElements(potentialCommonWords, 3);
+    let randomPotentialWords = getRandomElements(potentialWords, 3 - randomPotentialCommonWords.length);
+    let randomFinalPotentialWords = randomPotentialCommonWords.concat(randomPotentialWords);
 
-    document.getElementById('gameOverText').innerText = `Game Over. Your score was ${score}. You got out on the prompt: ${wordPrompt}. You could have used: ${randomPotentialWords.join(', ')}. Press Enter to play again.`;
+    document.getElementById('timer').innerText = 'Out of time';
+    document.getElementById('gameOverText').innerText = `Game Over.\nYour score was ${score}.\nYou got out on the prompt '${wordPrompt}'.\nYou could have used: ${randomFinalPotentialWords.join(', ')}.\nPress Enter to play again.`;
 }
+
